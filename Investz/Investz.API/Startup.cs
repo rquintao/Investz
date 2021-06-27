d944aebe5e463a6b510947ecd8110cfcf43c8e9d
Rafael Quintão <rafaelerquintao@gmail.com>
2021-06-28 00:16:11 +0100

    Added repository and service dynamic registration

--- Investz/Investz.API/Startup.cs
+++ Investz/Investz.API/Startup.cs
@@ -9,6 +9,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Investz
 {
@@ -25,10 +29,13 @@ namespace Investz
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IAuthenticationService, AuthenticationService>();
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<IUserRepository, UserRepository>();
+            //Register repositories
+            RegisterRepositories(services);
+
+            //Register services
+            RegisterServices(services);
+
+            //Register Context
             services.AddScoped<ICoreContext, CoreContext>();
 
             // Add Cors
@@ -45,7 +52,6 @@ namespace Investz
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Investz", Version = "v1" });
             });
 
-            //services.AddAsymmetricAuthentication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -72,6 +78,45 @@ namespace Investz
                 endpoints.MapControllers();
             });
 
+        }
+
+        private static void RegisterRepositories(IServiceCollection services) 
+        {
+            Assembly assemblyServices = Assembly.GetAssembly(typeof(UserRepository));
+            Assembly assemblyInterfaces = Assembly.GetAssembly(typeof(IUserRepository));
+
+            Assembly[] arr = { assemblyInterfaces };
+            Assembly[] repArr = { assemblyServices };
+
+            RegisterProcedure("Repository", arr, repArr, services, ServiceLifetime.Transient);
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+
+            Assembly assemblyServices = Assembly.GetAssembly(typeof(UserService));
+            Assembly assemblyInterfaces = Assembly.GetAssembly(typeof(IUserService));
+
+            Assembly[] arr = { assemblyInterfaces };
+            Assembly[] servArr = { assemblyServices };
+
+            RegisterProcedure("Service", arr, servArr, services, ServiceLifetime.Transient);
+        }
+
+        private static void RegisterProcedure(string type, Assembly[] interfaceAssemby, Assembly[] servAssembly, IServiceCollection services, ServiceLifetime serviceLifetime) 
+        {
+            IEnumerable<Type> interfaces = interfaceAssemby.SelectMany(x => x.GetTypes().Where(z => z.IsInterface && z.Name.EndsWith(type)));
+
+            foreach (Type t in interfaces)
+            {
+
+                IEnumerable<Type> serviceList = servAssembly.SelectMany(serv => serv.GetTypes().Where(x => x.GetInterfaces().Any(inter => inter.Name == t.Name)));
+
+                foreach (Type st in serviceList)
+                {
+                    services.Add(new ServiceDescriptor(t, st, serviceLifetime));
+                }
+            }
         }
     }
 }
